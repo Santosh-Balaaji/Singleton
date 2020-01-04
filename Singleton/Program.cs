cf0a5171ef7d31e48732ead0db246967a50e77ad
Santosh Balaaji <sv.santosh2695@gmail.com>
2020-01-05 11:51:13 +1300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Program
+    {
+        public  DateTime Datetime { get; set; }
+        private static readonly Lazy<Program> lazy = new Lazy<Program>(() => new Program(DateTime.UtcNow)); 
+        private  Program(DateTime datetime)
+        {
+            Datetime = datetime;
+        }
+
+        public static Program Instance {
+            get {
+                return lazy.Value;
+                }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Program.Instance.Datetime);
+        }
+    }
+}
